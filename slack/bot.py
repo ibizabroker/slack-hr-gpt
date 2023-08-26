d11ee93083aaf5e48741f4ea12033b0274446cfc
tarun gowda <tarun.gowda2000@gmail.com>
2023-08-26 13:27:06 +0530

    reaction event

--- slack/bot.py
+++ slack/bot.py
@@ -17,5 +17,26 @@ def handle_message_events(event, ack):
     response_using_llm(event, ack, app)
   return
 
+user_reactions = {}
+
+@app.event("reaction_added")
+def handle_reaction(event, say):
+  user_id = event["user"]
+  reaction = event["reaction"]
+  user_name = app.client.users_info(
+    user=user_id
+  )['user']['profile']['real_name']
+
+  if user_id not in user_reactions:
+    user_reactions[user_id] = reaction
+    if reaction == '+1':
+      response_text = f"Thank you {user_name} for your valued feedback!"
+    elif reaction == '-1':
+      response_text = f"Thank you {user_name} for your valued feedback!"
+    say(response_text)
+  else:
+    response_text = f"Hi {user_name}, looks like you've already provided a feedback!"
+    say(response_text)
+
 def run_bot():
   SocketModeHandler(app, app_token).start()
